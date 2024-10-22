e8e1584cf3939a654fabdb4c1b8793a9fa0ab27c
Pishta Oleksandr <live.gp@gmail.com>
2024-10-22 23:18:18 +0300

    add

---
+++ vite.config.js
@@ -0,0 +1,141 @@
+import TurboConsole from 'unplugin-turbo-console/vite';
+import { defineConfig, loadEnv } from 'vite';
+import { imagetools } from 'vite-imagetools';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
+import injectHtml from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import ogPlugin from 'vite-plugin-open-graph';
+import { svgSpritemap } from 'vite-plugin-svg-spritemap';
+import { webfontDownload } from 'vite-plugin-webfont-dl';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    define: {
+      'process.env': env,
+    },
+    base: env.VITE_BASE_URL || '/',
+    server: {
+      port: mode === 'development' ? 3000 : 8080,
+    },
+    build: {
+      outDir: mode === 'production' ? 'dist' : 'build',
+      sourcemap: true,
+    },
+    plugins: [
+      ViteMinifyPlugin({}),
+      TurboConsole({}),
+      // biomePlugin({
+      //   mode: 'check',
+      //   files: '.',
+      //   applyFixes: true,
+      //   failOnError: false,
+      // }),
+      webfontDownload(
+        [
+          'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap',
+        ],
+        {
+          injectAsStyleTag: true,
+          minifyCss: true,
+          embedFonts: false,
+          async: true,
+          cache: true,
+          proxy: false,
+        },
+      ),
+      imagetools({
+        defaultDirectives: () => {
+          return new URLSearchParams({
+            format: 'avif;webp;jpg',
+            quality: '75',
+          });
+        },
+        cache: {
+          enabled: true,
+          dir: './node_modules/.cache/imagetools',
+        },
+      }),
+      svgSpritemap({
+        pattern: 'src/icons/*.svg',
+        prefix: '',
+        filename: 'spritemap.svg',
+        currentColor: false,
+        svgo: true,
+        emit: false,
+      }),
+      vitePluginFaviconsInject('src/favicons/favicons.svg', {
+        path: '/',
+        appName: 'resume-new',
+        appShortName: 'resume-new',
+        appDescription: 'the starting template of the project',
+        developerName: 'Oleksandr Pishta',
+        // biome-ignore lint/style/useNamingConvention: <explanation>
+        developerURL: 'https://livegp.github.io',
+        cacheBustingQueryParam: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        // biome-ignore lint/style/useNamingConvention: <explanation>
+        theme_color: '#fff',
+        appleStatusBarStyle: 'black-translucent',
+        display: 'standalone',
+        orientation: 'any',
+        scope: '/',
+        // biome-ignore lint/style/useNamingConvention: <explanation>
+        start_url: '/?homescreen=1',
+        preferRelatedApplications: false,
+        relatedApplications: undefined,
+        version: '1.0',
+        // biome-ignore lint/style/useNamingConvention: <explanation>
+        pixel_art: false,
+        loadManifestWithCredentials: false,
+        manifestMaskable: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          favicons: true,
+          windows: true,
+          yandex: false,
+        },
+        failGraciously: false,
+      }),
+      ogPlugin({
+        basic: {
+          url: 'https://livegp.github.io/resume-new/',
+          siteName: 'resume-new',
+          title: 'resume-new',
+          description: 'Oleksandr Pishta, Full Stack Developer',
+          type: 'image.png',
+          determiner: 'auto',
+          locale: 'en_US',
+          image: {
+            url: 'https://livegp.github.io/resume-new/og.jpg',
+            type: 'image/png',
+            width: 900,
+            height: 440,
+            alt: 'Vite Image',
+          },
+        },
+        twitter: {
+          card: 'summary_large_image',
+          site: '@live_gp',
+          siteId: '118488504',
+          creator: '@live_gp',
+          creatorId: '118488504',
+          description: 'Oleksandr Pishta, Full Stack Developer',
+          title: 'resume-new',
+          image: 'https://livegp.github.io/resume-new/og.jpg',
+          imageAlt: 'Vite Image',
+        },
+        facebook: {
+          appId: '100000506117116',
+        },
+      }),
+      injectHtml(),
+    ],
+  };
+});
